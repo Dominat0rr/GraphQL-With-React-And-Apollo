533628baf6f9674e1ffc75e5ee276e618a094cea
Dominat0rr <claermandominik@gmail.com>
2020-07-08 00:21:24 +0200

    Update project for hosting/deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const schema = require("./schema");
+const path = require("path");
 const app = express();
 
 // Allow cros-origin
@@ -13,5 +14,11 @@ app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
