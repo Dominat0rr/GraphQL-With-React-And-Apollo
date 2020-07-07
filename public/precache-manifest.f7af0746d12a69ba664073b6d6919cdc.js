66c44eff80eca32a2dcc3e4d6c35fd0aa16c0fbe
Dominat0rr <claermandominik@gmail.com>
2020-07-08 00:24:09 +0200

    Add public folder for hosting

---
+++ public/precache-manifest.f7af0746d12a69ba664073b6d6919cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ca50fa1fa1786f820a121abc6571ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfb3df9f6109968abd64",
+    "url": "/static/css/main.60a3e41e.chunk.css"
+  },
+  {
+    "revision": "8e72f0519bb03a170243",
+    "url": "/static/js/2.826e6c8a.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.826e6c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb3df9f6109968abd64",
+    "url": "/static/js/main.970756ea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
